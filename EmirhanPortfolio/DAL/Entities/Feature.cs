38723e3380eea0e43013605dce458895e6cf54c4
emirhanselcuk <selcukemirhan88@gmail.com>
2024-10-14 11:19:07 +0300

    CreateFeature

---
+++ EmirhanPortfolio/DAL/Entities/Feature.cs
@@ -0,0 +1,10 @@
+﻿namespace EmirhanPortfolio.DAL.Entities
+{
+    public class Feature
+    {
+        //Feature
+        public int FeatureId { get; set; }
+        public string FeatureDescription { get; set; }
+        public string Title { get; set; }
+    }
+}
