fadad4d045754ae2747b1fc99187cd1356c25ebb
emirhanselcuk <selcukemirhan88@gmail.com>
2024-10-14 14:59:43 +0300

    Feature verilerinin Db'den çekilmesi

---
+++ EmirhanPortfolio/ViewComponents/_FeatureComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using EmirhanPortfolio.DAL.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmirhanPortfolio.ViewComponents
+{
+    public class _FeatureComponentPartial:ViewComponent
+    {
+        MyPortfolioContext portfolioContext;
+        public IViewComponentResult Invoke()
+        {
+            var values = portfolioContext.Features.ToList();
+            return View(values);
+        }
+    }
+}
