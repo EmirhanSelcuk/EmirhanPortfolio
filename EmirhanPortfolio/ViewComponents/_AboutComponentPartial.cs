355690848c75e93d39fc68f8843ff492ecd4968d
emirhanselcuk <selcukemirhan88@gmail.com>
2024-10-14 15:40:44 +0300

    About verilerinin Db'den çekilmesi

---
+++ EmirhanPortfolio/ViewComponents/_AboutComponentPartial.cs
@@ -0,0 +1,17 @@
+﻿using EmirhanPortfolio.DAL.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmirhanPortfolio.ViewComponents
+{
+    public class _AboutComponentPartial:ViewComponent
+    {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.aboutTitle = portfolioContext.Abouts.Select(x=>x.Title).FirstOrDefault();
+            ViewBag.aboutSubDescription = portfolioContext.Abouts.Select(x=>x.SubDescription).FirstOrDefault();
+            ViewBag.aboutDetail = portfolioContext.Abouts.Select(x=>x.Details).FirstOrDefault();
+            return View();
+        }
+    }
+}
