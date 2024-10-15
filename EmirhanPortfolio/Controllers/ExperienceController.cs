d8962705babdd54867f6918b6c95f6bc4559e7bf
emirhanselcuk <selcukemirhan88@gmail.com>
2024-10-15 10:19:44 +0300

    Experince Admin Panel Düzenleme

---
+++ EmirhanPortfolio/Controllers/ExperienceController.cs
@@ -0,0 +1,15 @@
+﻿using EmirhanPortfolio.DAL.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmirhanPortfolio.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IActionResult ExperinceList()
+        {
+            var values = context.Experiences.ToList();
+            return View(values);
+        }
+    }
+}
