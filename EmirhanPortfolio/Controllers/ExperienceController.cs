ead2d44d716d2f19e59616e75a9de75e3b14c353
emirhanselcuk <selcukemirhan88@gmail.com>
2024-10-15 11:41:41 +0300

    DeleteExperience

--- EmirhanPortfolio/Controllers/ExperienceController.cs
+++ EmirhanPortfolio/Controllers/ExperienceController.cs
@@ -1,4 +1,5 @@
 ﻿using EmirhanPortfolio.DAL.Context;
+using EmirhanPortfolio.DAL.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmirhanPortfolio.Controllers
@@ -6,10 +7,31 @@ namespace EmirhanPortfolio.Controllers
     public class ExperienceController : Controller
     {
         MyPortfolioContext context = new MyPortfolioContext();
-        public IActionResult ExperinceList()
+        public IActionResult ExperienceList()
         {
             var values = context.Experiences.ToList();
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateExperience()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            context.Experiences.Add(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        public IActionResult DeleteExperience(int id)
+        {
+            var value = context.Experiences.Find(id);
+            context.Experiences.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
         }
     }
 }
